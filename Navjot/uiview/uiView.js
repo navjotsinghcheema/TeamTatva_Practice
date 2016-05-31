528bd1d8455b52d75e95ce3e9cf46b2dc2d7e813
navjotsinghcheema <navjotsinghcheema>
2016-05-31 12:54:22 +0000

    navjot 31-05-2016

---
+++ Navjot/uiview/uiView.js
@@ -0,0 +1,22 @@
+angular.module('myApp',['ui.router'])
+  .config(function($stateProvider,$urlRouterProvider){
+    $urlRouterProvider.otherwise('/teams')
+  $stateProvider.state('teams',{
+    url:'/teams',
+    templateUrl:'teams.html',
+    controller: function($scope){
+      $scope.allTeams=["Tatva","QuizRT","buggers"];
+    }
+  })
+  .state('teams.team1',{
+    url:'/team:Tatva',
+    templateUrl:'team.html',
+    controller:$scope.members=["nav","tapas","gurleen"]
+  })
+  .state('teams.team1.member',{
+    url:'/member',
+    templateUrl:'member.html',
+    controller:'getMember'
+  })
+);
+}
